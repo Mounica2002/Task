13f1bb20db8fad65660b7a681cec57b48fb301ff
Mounica2002 <106916614+Mounica2002@users.noreply.github.com>
2023-03-15 12:29:56 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -1,5 +1,42 @@
 <?php  
-    if(file_exists("database/".$username.".txt")){
+include 'config.php';
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+   $image = $_FILES['image']['name'];
+   $image_size = $_FILES['image']['size'];
+   $image_tmp_name = $_FILES['image']['tmp_name'];
+   $image_folder = 'uploaded_img/'.$image;
+
+   $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select) > 0){
+      $message[] = 'user already exist'; 
+   }else{
+      if($pass != $cpass){
+         $message[] = 'confirm password not matched!';
+      }elseif($image_size > 1000000){
+         $message[] = 'image size is too large!';
+      }else{
+         $insert = mysqli_query($conn, "INSERT INTO `user_form`(name, email, password, image) VALUES('$name', '$email', '$pass', '$image')") or die('query failed');
+
+         if($insert){
+            move_uploaded_file($image_tmp_name, $image_folder);
+            $message[] = 'registered successfully!';
+            header('location:login.php');
+         }else{
+            $message[] = 'registeration failed!';
+         }
+      }
+   }
+
+}
+
+if(file_exists("database/".$username.".txt")){
         
         $file = fopen("database/".$username.".txt","r");
         $username = fgets($file);
